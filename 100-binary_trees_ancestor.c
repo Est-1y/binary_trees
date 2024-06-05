ec8ca6d727840df2a0a8815775fff8d5b7df868f
Est-1y <terryiana22@gmail.com>
2024-06-05 02:32:18 -0700

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - function for lowest common ancestor
+ * @first: first tree node.
+ * @second: Second tree node
+ * Return: the node(ancestor)
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *p, *q;
+
+	if (first == NULL || second == NULL)
+	{
+		return (NULL);
+	}
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
+
+	p = first->parent;
+	q = second->parent;
+	if (p == NULL || first == q || (!p->parent && q))
+	{
+		return (binary_trees_ancestor(first, q));
+	}
+	else if (q == NULL || p == second || (!q->parent && p))
+	{
+		return (binary_trees_ancestor(p, second));
+	}
+	return (binary_trees_ancestor(p, q));
+}
